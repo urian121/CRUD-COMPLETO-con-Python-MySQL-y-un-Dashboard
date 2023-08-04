10e3b1cdaf0739ed4f40a0c808c6a8478f58bc6d
Urian Viera <urian1213viera@gmail.com>
2023-08-04 08:32:20 -0500

    agregando bd

--- my-app/BD/crud_python.sql
+++ my-app/BD/crud_python.sql
@@ -19,53 +19,26 @@
 CREATE DATABASE IF NOT EXISTS `crud_python` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `crud_python`;
 
--- Volcando estructura para tabla crud_python.consignaciones
-CREATE TABLE IF NOT EXISTS `consignaciones` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `valor_consignacion` bigint NOT NULL,
-  `name_archivo` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `url_archivo` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nota_consignacion` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `estatus_leido` tinyint(1) DEFAULT '0',
-  `bandeja` bigint DEFAULT '0',
-  `code_tienda` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `ip_consignacion` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nombre_tienda` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `status_consignacion_ignorada` int DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla crud_python.consignaciones: ~10 rows (aproximadamente)
-
--- Volcando estructura para tabla crud_python.detalles_consignaciones
-CREATE TABLE IF NOT EXISTS `detalles_consignaciones` (
-  `id_detalles_consignaciones` int NOT NULL AUTO_INCREMENT,
-  `id_consignacion` int DEFAULT NULL,
-  `valor_venta` int DEFAULT NULL,
-  `dia_venta` date DEFAULT NULL,
-  `fecha_consignacion_banco` date DEFAULT NULL,
-  `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_detalles_consignaciones`),
-  KEY `FK_CONSIGNACION` (`id_consignacion`),
-  CONSTRAINT `FK_CONSIGNACION` FOREIGN KEY (`id_consignacion`) REFERENCES `consignaciones` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla crud_python.detalles_consignaciones: ~14 rows (aproximadamente)
-
 -- Volcando estructura para tabla crud_python.tbl_empleados
 CREATE TABLE IF NOT EXISTS `tbl_empleados` (
-  `id_empleado` int DEFAULT NULL,
+  `id_empleado` int NOT NULL AUTO_INCREMENT,
   `nombre_empleado` varchar(50) DEFAULT NULL,
   `apellido_empleado` varchar(50) DEFAULT NULL,
   `sexo_empleado` int DEFAULT NULL,
   `telefono_empleado` varchar(50) DEFAULT NULL,
   `email_empleado` varchar(50) DEFAULT NULL,
-  `profesion_empleado` int DEFAULT NULL,
+  `profesion_empleado` varchar(50) DEFAULT NULL,
   `foto_empleado` mediumtext,
-  `cargo_empleado` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `salario_empleado` bigint DEFAULT NULL,
+  `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_empleado`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla crud_python.tbl_empleados: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla crud_python.tbl_empleados: ~3 rows (aproximadamente)
+INSERT INTO `tbl_empleados` (`id_empleado`, `nombre_empleado`, `apellido_empleado`, `sexo_empleado`, `telefono_empleado`, `email_empleado`, `profesion_empleado`, `foto_empleado`, `salario_empleado`, `fecha_registro`) VALUES
+	(1, 'urian', 'viera', 1, '32423432', 'urian@gmail.com', 'Ing', 'b144e61f05d24aa092f9d016465bd16ee18ac1b3d6df4fe3affcadcba22becd7.jpeg', 1, '2023-08-02 20:09:25'),
+	(2, 'Uriany', 'Viera', 1, '434234', 'uriany@gmail.com', 'Ing', '719b21f6abcd4a4380da8596337bd0f91d34adb23d6f422daea391bc3d4f5079.png', 2543000, '2023-08-02 21:26:51'),
+	(3, 'Josefina', 'Viera', 2, '4324', 'j@gmail.com', 'Doctora', 'bb48abdbddbf4ac1a27ad92f3c501a2c47dde60bd35b41b78e969965c6dbdd18.png', 34567657, '2023-08-02 21:45:33');
 
 -- Volcando estructura para tabla crud_python.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -77,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla crud_python.users: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla crud_python.users: ~2 rows (aproximadamente)
 INSERT INTO `users` (`id`, `name_surname`, `email_user`, `pass_user`, `created_user`) VALUES
 	(1, 'Urian', 'dev@gmail.com', 'scrypt:32768:8:1$ZXqvqovbXYQZdrAB$66758083429739f4f8985992b22cb89fb58c04b99010858e7fb26f73078a23dd3e16019a17bf881108d582a91a635d2c21d26d80da1612c2d9c9bbb9b06452dc', '2023-07-21 20:10:01'),
 	(2, 'demo', 'demo@gmail.com', 'scrypt:32768:8:1$Yl2tGU1Ru1Q4Jrzq$d88a0ded538dcfc3a01c8ebf4ea77700576203f6a7cc765f04627464c6047bdcf8eaad84ca3cf0bb5ed058d2dff8ee7a0ba690803538764bedc3ba6173ac6a8a', '2023-07-21 20:29:28');
